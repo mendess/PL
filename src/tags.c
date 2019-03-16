84ba170140a909302870b3e941058d46c1c1eae3
Mendess2526 <pedro.mendes.26@gmail.com>
2019-03-16 15:17:48 +0000

    Reformat code

--- src/tags.c
+++ src/tags.c
@@ -1,28 +1,34 @@
 #include <glib.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 static GHashTable* tags = NULL;
 
-static int vstrcmp(const void* a, const void* b) {
+static int vstrcmp(const void* a, const void* b)
+{
     return strcmp((char*)a, (char*)b);
 }
 
-void tags_init() {
+void tags_init()
+{
     tags = g_hash_table_new_full(g_str_hash, vstrcmp, free, NULL);
 }
 
-void tags_add(char* tag) {
-    g_hash_table_add(tags, g_strdup(tag));
+void tags_add(char* tag)
+{
+    if (!g_hash_table_lookup(tags, tag)) {
+        g_hash_table_add(tags, g_strdup(tag));
+    }
 }
 
-void tags_flush() {
-    FILE* f = fopen("tags.txt", "w");
+void tags_flush()
+{
+    FILE* f = fopen("noticias/tags.txt", "w");
     GHashTableIter iter;
     gpointer key;
     g_hash_table_iter_init(&iter, tags);
-    while(g_hash_table_iter_next(&iter, &key, NULL)) {
+    while (g_hash_table_iter_next(&iter, &key, NULL)) {
         fprintf(f, "%s\n", (char*)key);
     }
     g_hash_table_destroy(tags);
