af33eaf18e5a9383156f3fbd4d4236a9dd3464e0
Mendess2526 <pedro.mendes.26@gmail.com>
2019-04-09 12:09:45 +0100

    Add base

---
+++ TP2/scripts/get_input.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"/.. || exit 1
+
+mkdir -p input
+cd input || exit 0
+
+curl http://natura.di.uminho.pt/\~jj/pl-18/TP1/CORPORA1/ \
+    | grep td \
+    | awk -F'=' '{print $5}' \
+    | awk -F'"' '{print $2}' \
+    | tail -3 \
+    | sed 's|^|http://natura.di.uminho.pt/\~jj/pl-18/TP1/CORPORA1/|' \
+    | xargs wget
+xz -d ./*.xz
